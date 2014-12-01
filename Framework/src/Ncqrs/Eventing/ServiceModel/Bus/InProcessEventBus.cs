510a613628c00cf22dff52cfbccade03236ed27a
Oleg Temnov <otemnov@academixdirect.com>
2014-12-01 16:02:44 +0300

    InProcessEventBus optimization

--- Framework/src/Ncqrs/Eventing/ServiceModel/Bus/InProcessEventBus.cs
+++ Framework/src/Ncqrs/Eventing/ServiceModel/Bus/InProcessEventBus.cs
@@ -1,139 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System.Linq;
 using System.Reflection;
 using System.Transactions;
 
 namespace Ncqrs.Eventing.ServiceModel.Bus
 {
-    public class InProcessEventBus : IEventBus
-    {
-        private readonly Dictionary<Type, List<Action<PublishedEvent>>> _handlerRegister = new Dictionary<Type, List<Action<PublishedEvent>>>();
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-        private readonly bool _useTransactionScope;
+	public class InProcessEventBus : IEventBus
+	{
+		private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        /// <summary>
-        /// Creates new <see cref="InProcessEventBus"/> instance that wraps publishing to
-        /// handlers into a <see cref="TransactionScope"/>.
-        /// </summary>
-        public InProcessEventBus()
-            : this(true)
-        {            
-        }
+		private readonly Dictionary<Type, List<Action<PublishedEvent>>> _handlerRegister =
+			new Dictionary<Type, List<Action<PublishedEvent>>>();
 
-        /// <summary>
-        /// Creates new <see cref="InProcessEventBus"/> instance.
-        /// </summary>
-        /// <param name="useTransactionScope">Use transaction scope?</param>
-        public InProcessEventBus(bool useTransactionScope)            
-        {
-            _useTransactionScope = useTransactionScope;
-        }
+		private readonly bool _useTransactionScope;
 
-        public void Publish(IPublishableEvent eventMessage)
-        {
-            var eventMessageType = eventMessage.GetType();
+		/// <summary>
+		///     Creates new <see cref="InProcessEventBus" /> instance that wraps publishing to
+		///     handlers into a <see cref="TransactionScope" />.
+		/// </summary>
+		public InProcessEventBus()
+			: this(true)
+		{
+		}
 
-            Log.InfoFormat("Started publishing event {0}.", eventMessageType.FullName);
+		/// <summary>
+		///     Creates new <see cref="InProcessEventBus" /> instance.
+		/// </summary>
+		/// <param name="useTransactionScope">Use transaction scope?</param>
+		public InProcessEventBus(bool useTransactionScope)
+		{
+			_useTransactionScope = useTransactionScope;
+		}
 
-            IEnumerable<Action<PublishedEvent>> handlers = GetHandlersForEvent(eventMessage);
+		public void Publish(IPublishableEvent eventMessage)
+		{
+			Type eventMessageType = eventMessage.GetType();
 
-            if (handlers.Count() == 0)
-            {
-                Log.WarnFormat("Did not found any handlers for event {0}.", eventMessageType.FullName);
-            }
-            else
-            {
-                if (_useTransactionScope)
-                {
-                    TransactionallyPublishToHandlers(eventMessage, eventMessageType, handlers);
-                }
-                else
-                {
-                    PublishToHandlers(eventMessage, eventMessageType, handlers);
-                }
-            }
-        }
+			Log.InfoFormat("Started publishing event {0}.", eventMessageType.FullName);
 
-        private static void TransactionallyPublishToHandlers(IPublishableEvent eventMessage, Type eventMessageType, IEnumerable<Action<PublishedEvent>> handlers)
-        {
-            Contract.Requires<ArgumentNullException>(handlers != null);
+			IReadOnlyCollection<Action<PublishedEvent>> handlers = GetHandlersForEvent(eventMessage);
 
-            using (var transaction = new TransactionScope())
-            {
-                PublishToHandlers(eventMessage, eventMessageType, handlers);
-                transaction.Complete();
-            }
-        }
+			if (handlers.Count == 0)
+			{
+				Log.WarnFormat("Did not found any handlers for event {0}.", eventMessageType.FullName);
+			}
+			else
+			{
+				if (_useTransactionScope)
+				{
+					TransactionallyPublishToHandlers(eventMessage, eventMessageType, handlers);
+				}
+				else
+				{
+					PublishToHandlers(eventMessage, eventMessageType, handlers);
+				}
+			}
+		}
 
-        private static void PublishToHandlers(IPublishableEvent eventMessage, Type eventMessageType, IEnumerable<Action<PublishedEvent>> handlers)
-        {
-            Contract.Requires<ArgumentNullException>(handlers != null);
+		public void Publish(IEnumerable<IPublishableEvent> eventMessages)
+		{
+			foreach (IPublishableEvent eventMessage in eventMessages)
+			{
+				Publish(eventMessage);
+			}
+		}
 
-            Log.DebugFormat("Found {0} handlers for event {1}.", handlers.Count(), eventMessageType.FullName);
+		private static void TransactionallyPublishToHandlers(IPublishableEvent eventMessage, Type eventMessageType, IReadOnlyCollection<Action<PublishedEvent>> handlers)
+		{
+			Contract.Requires<ArgumentNullException>(handlers != null);
 
-            var publishedEventClosedType = typeof (PublishedEvent<>).MakeGenericType(eventMessage.Payload.GetType());
-            var publishedEvent = (PublishedEvent)Activator.CreateInstance(publishedEventClosedType, eventMessage);
+			using (var transaction = new TransactionScope())
+			{
+				PublishToHandlers(eventMessage, eventMessageType, handlers);
+				transaction.Complete();
+			}
+		}
 
-            foreach (var handler in handlers)
-            {
-                Log.DebugFormat("Calling handler {0} for event {1}.", handler.GetType().FullName,
-                                eventMessageType.FullName);
+		private static void PublishToHandlers(IPublishableEvent eventMessage, Type eventMessageType, IReadOnlyCollection<Action<PublishedEvent>> handlers)
+		{
+			Log.DebugFormat("Found {0} handlers for event {1}.", handlers.Count, eventMessageType.FullName);
 
-                handler(publishedEvent);
+			Type publishedEventClosedType = typeof (PublishedEvent<>).MakeGenericType(eventMessage.Payload.GetType());
+			var publishedEvent = (PublishedEvent) Activator.CreateInstance(publishedEventClosedType, eventMessage);
 
-                Log.DebugFormat("Call finished.");
-            }
-        }
+			foreach (var handler in handlers)
+			{
+				Log.DebugFormat("Calling handler {0} for event {1}.", handler.GetType().FullName, eventMessageType.FullName);
 
-        [ContractVerification(false)]
-        protected IEnumerable<Action<PublishedEvent>> GetHandlersForEvent(IPublishableEvent eventMessage)
-        {
-            if (eventMessage == null)
-                return null;
+				handler(publishedEvent);
 
-            var dataType = eventMessage.Payload.GetType();
-            var result = new List<Action<PublishedEvent>>();
+				Log.DebugFormat("Call finished.");
+			}
+		}
 
-            foreach(var key in _handlerRegister.Keys)
-            {
-                if(key.IsAssignableFrom(dataType))
-                {
-                    var handlers = _handlerRegister[key];
-                    result.AddRange(handlers);
-                }
-            }
+		protected IReadOnlyCollection<Action<PublishedEvent>> GetHandlersForEvent(IPublishableEvent eventMessage)
+		{
+			Type dataType = eventMessage.Payload.GetType();
+			return _handlerRegister[dataType].AsReadOnly();
+		}
 
-            return result;
-        }
+		public void RegisterHandler<TEvent>(IEventHandler<TEvent> handler)
+		{
+			Type eventDataType = typeof(TEvent);
+			Action<PublishedEvent> act = evnt => handler.Handle((IPublishedEvent<TEvent>) evnt);
+			RegisterHandler(eventDataType, act);
+		}
 
-        public void Publish(IEnumerable<IPublishableEvent> eventMessages)
-        {
-            foreach (var eventMessage in eventMessages)
-            {
-                Publish(eventMessage);
-            }
-        }
+		public void RegisterHandler(Type eventDataType, Action<PublishedEvent> handler)
+		{
+			List<Action<PublishedEvent>> handlers;
+			if (!_handlerRegister.TryGetValue(eventDataType, out handlers))
+			{
+				handlers = new List<Action<PublishedEvent>>(1);
+				_handlerRegister.Add(eventDataType, handlers);
+			}
 
-        public void RegisterHandler<TEvent>(IEventHandler<TEvent> handler)
-        {
-            var eventDataType = typeof(TEvent);
-
-            Action<PublishedEvent> act = evnt => handler.Handle((IPublishedEvent<TEvent>)evnt);
-            RegisterHandler(eventDataType, act);
-        }
-
-        public void RegisterHandler(Type eventDataType, Action<PublishedEvent> handler)
-        {
-            List<Action<PublishedEvent>> handlers = null;
-            if (!_handlerRegister.TryGetValue(eventDataType, out handlers))
-            {
-                handlers = new List<Action<PublishedEvent>>(1);
-                _handlerRegister.Add(eventDataType, handlers);
-            }
-
-            handlers.Add(handler);
-        }
-    }
+			handlers.Add(handler);
+		}
+	}
 }
